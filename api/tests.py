203ab5ec4c6d6664be56846d8094e6ec4c4062a9
puribe <puribe@puribe.cl>
2025-04-23 21:16:56 -0400

    a ver si mejora

--- api/tests.py
+++ api/tests.py
@@ -135,7 +135,7 @@ def test_create_medida(admin_client):
 
     res = admin_client.post("/api/medidas/", data, format="json")
     assert res.status_code == 201
-    assert res.data["nombre_corto"] == "Medida XYXdd"
+    assert res.data["nombre_corto"] == "Medida X"
 
 # ------------------------ MEDIDA REPORTADA ------------------------
 
